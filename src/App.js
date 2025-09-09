6d3c8a9c6fddcc5a8e125c26dd501399686fe2c4
Iyuu8 <anesbenaziza19@gmail.com>
2025-09-10 00:44:09 +0100

    bubble sort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const bubbleSort= async (arrStuff)=>{
+  const [arr,setArr]=arrStuff;
+  const tempArr=[...arr];
+  let swaped;
+  let c=0;
+  do{
+    c++;
+    swaped = false;
+    for(let i=0;i<arr.length-c;i++){
+      if(tempArr[i]>tempArr[i+1]){
+        swaped=true;
+        [tempArr[i],tempArr[i+1]]=[tempArr[i+1],tempArr[i]]
+        setArr([...tempArr]);
+        await new Promise(resolve=> setTimeout(resolve,10));
+      }
+    }
+  }while(swaped)
+}
+
+
+const randArr=(n)=>{
+  const arr=[];
+  for(let i=0;i<n;i++){
+    arr.push(Math.floor(Math.random()*1000));
+  }
+  return arr;
+}
 
 function App() {
+  const [arr,setArr]=useState(randArr(100));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sorting-container">
+        <div className="sorting-container-1">
+          <ul className="sorting-1">
+            {arr.map((item,ind)=>(
+              <li 
+                className="sort-item"
+                key={`sort1-${ind}`}
+                style={{
+                  backgroundColor:"green",
+                  width:`${Math.floor(item/3)}px`,
+                  height: '6px'
+                }}
+              ></li>
+            ))
+
+            }
+          </ul>
+          <button 
+            className="start-sort"
+            onClick={async ()=> bubbleSort([arr,setArr])}
+          > start sort 1</button>
+        </div>
+        <div className="sorting-container-2">
+          <ul className="sorting-2">
+
+          </ul>
+          <button className="start-sort"></button>
+
+        </div>
+      </div>
+      
     </div>
   );
 }
